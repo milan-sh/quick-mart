8a5ad764ba7301dfaae8fbea8ec7ddba3883604a
Milan Singh <milan083821@gmail.com>
2025-02-13 22:33:08 +0530

    fix footer style issue

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,47 +5,46 @@ import {
   faGithub,
   faLinkedin,
   faDiscord,
-  faTwitter
+  faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="footer grid grid-cols-2 md:grid-cols-4 gap-x-0 bg-secondaryBgColor text-white  p-10">
-        <nav>
+      <div className="footer grid justify-items-center gap-x-0 bg-secondaryBgColor text-white  p-10">
+        <nav className="justify-items-center md:justify-items-start">
           <h6 className="footer-title">Services</h6>
           <a className="link link-hover">Branding</a>
           <a className="link link-hover">Design</a>
           <a className="link link-hover">Marketing</a>
           <a className="link link-hover">Advertisement</a>
         </nav>
-        <nav>
+        <nav className="justify-items-center md:justify-items-start">
           <h6 className="footer-title">Company</h6>
           <a className="link link-hover">About us</a>
           <a className="link link-hover">Contact</a>
           <a className="link link-hover">Jobs</a>
           <a className="link link-hover">Press kit</a>
         </nav>
-        <nav>
+        <nav className="justify-items-center md:justify-items-start">
           <h6 className="footer-title">Legal</h6>
           <a className="link link-hover">Terms of use</a>
           <a className="link link-hover">Privacy policy</a>
           <a className="link link-hover">Cookie policy</a>
         </nav>
-        <form>
+        <form className="justify-items-center md:justify-items-start">
           <h6 className="footer-title">Newsletter</h6>
-          <fieldset className="form-control md:w-80">
-            <label className="label">
+          <fieldset className="form-control max-w-fit">
+            <label className="label justify-center md:justify-start">
               <span className="label-text">Enter your email address</span>
             </label>
-            <div className="join flex flex-col md:flex-row w-fit">
+            <div className="join">
               <input
-                type="email"
+                type="text"
                 placeholder="yourname@gmail.com"
-                className="input md:input-bordered border border-white join-item px-2 "
-                required
+                className="input input-bordered join-item"
               />
-              <button className="btn btn-primary join-item">Subscribe</button>
+              <button className="btn btn-primary hover:bg-primaryButtonColor join-item">Subscribe</button>
             </div>
           </fieldset>
         </form>
@@ -69,7 +68,7 @@ const Footer = () => {
             <FontAwesomeIcon size="xl" icon={faDiscord} />
           </a>
           <a href="https://x.com/dev_81milan" target="_blank">
-          <FontAwesomeIcon size="xl" icon={faTwitter} />
+            <FontAwesomeIcon size="xl" icon={faTwitter} />
           </a>
         </div>
       </div>
